d45a66bc546fa0323fca2e3dcd744ce2506ced5a
Joao <engjobe@gmail.com>
2021-12-02 09:49:42 -0300

    exercicio 4

--- exercicios/Exercicio04.java
+++ exercicios/Exercicio04.java
@@ -6,25 +6,31 @@ public class Exercicio04 {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        double salarioMinimo, kwConsumo;
+        double salarioMinimo, kwConsumido;
         double valorKW, valorTotal, valorComDesconto;
+        // final double TAXA_POR_KW = 500; // constant = não pode ser alterado
         
         // entrada
         System.out.println("Digite o valor do salário mínimo:");
         salarioMinimo = entrada.nextDouble();
-
-        System.out.println("Digite a quantidade de quilowatts consumida:");
-        kwConsumo = entrada.nextDouble();
+        System.out.println("Digite o consumo em KW:");
+        kwConsumido = entrada.nextDouble();
 
         // processamento
+        // cada KW custa 1 / 500 do salário mínimo
+        // valorKW = salarioMinimo / TAXA_POR_KW;
+        //valorComDesconto = valorTotal - valorTotal * 0.15;
         valorKW = salarioMinimo / 500;
-        valorTotal = valorKW * kwConsumo;
-        valorComDesconto = valorTotal - valorTotal * 0.15;
+        valorTotal = valorKW * kwConsumido;
+        valorComDesconto = valorTotal * 0.85;
 
         // saída
-        System.out.println("O valor, em reais, de cada quilowatt: " + valorKW);
-        System.out.println("O valor, em reais, a ser pago por essa residência: " + valorTotal);
-        System.out.println("O valor, em reais, a ser pago com desconto de 15%: " + valorComDesconto);
+        // System.out.println("O valor, em reais, de cada quilowatt: " + valorKW);
+        // System.out.println("O valor, em reais, a ser pago por essa residência: " + valorTotal);
+        // System.out.println("O valor, em reais, a ser pago com desconto de 15%: " + valorComDesconto);
+        System.out.println("1 Kw custa: " + valorKW);
+        System.out.println("Valor Total: " + valorTotal);
+        System.out.println("Valor com 15% de desconto: " + valorComDesconto);
 
         entrada.close();
     } 

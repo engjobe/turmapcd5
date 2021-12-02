c20985ac6e95239771ba71cab17c5799d6d631ea
Joao <engjobe@gmail.com>
2021-12-02 14:13:16 -0300

    exercicio 03

--- aula04/exercicios/Exercicio03.java
+++ aula04/exercicios/Exercicio03.java
@@ -16,7 +16,8 @@ public class Exercicio03 {
         if(numero1 < numero2) {
             // System.out.println(numero1 + ", " + numero2);
             System.out.printf("%d, %d\n", numero1, numero2);
-        } else {System.out.println(numero2 + ", " + numero1);
+        } else {
+            System.out.println(numero2 + ", " + numero1);
 
         }
 

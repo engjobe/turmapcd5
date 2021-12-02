bf52c04d4937eb09794b28e618e32bcd2ef717e3
Joao <engjobe@gmail.com>
2021-12-01 21:00:20 -0300

    aula 03

---
+++ exercicios/Exercicio01.java
@@ -0,0 +1,23 @@
+package exercicios;
+
+public class Exercicio01 {
+
+    public static void main(String[] args) {
+        double resultado;
+
+        resultado = 15 + 7;
+        System.out.println(resultado);
+
+        resultado = 15 - 7;
+        System.out.println(resultado);
+
+        resultado = 15 * 7;
+        System.out.println(resultado);
+
+        resultado = 15 / 7;
+        System.out.println(resultado); // int / int ==> resultado será int
+
+        resultado = 15.0 / 7;
+        System.out.println(resultado); // double / int ==> resultado será double
+    } 
+}

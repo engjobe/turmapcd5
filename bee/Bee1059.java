2b8e92c8a258e64c4d9d553156fdce41868301b9
Joao <engjobe@gmail.com>
2021-12-05 00:23:19 -0300

    Bee1059

---
+++ bee/Bee1059.java
@@ -0,0 +1,11 @@
+public class Bee1059 { // Main
+   
+   public static void main(String[] args) {
+      
+      int cont;
+
+      for (cont = 2; cont <= 100; cont = cont + 2) {
+         System.out.println(cont);
+      }
+   }
+}
---
+++ bee/Bee1059A.java
@@ -0,0 +1,14 @@
+public class Bee1059A { // Main
+
+    public static void main(String[] args) {
+
+        int cont;
+
+        cont = 2;
+
+        while (cont <= 100) {
+            System.out.println(cont);
+            cont = cont + 2;
+        }
+    }
+}

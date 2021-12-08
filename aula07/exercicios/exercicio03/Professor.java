a1a9cea0dac856658d1b5fd9ec17d7cf7160f00a
Joao <engjobe@gmail.com>
2021-12-07 21:03:38 -0300

    aula 07

--- aula07/exercicios/exercicio03/Professor.java
+++ aula07/exercicios/exercicio03/Professor.java
@@ -1,5 +1,15 @@
 package exercicios.exercicio03;
 
-public class Professor {
+public class Professor extends Pessoa {
+    private double salario;
+
+    public Professor(String nome, String telefone, double salario) {
+        super(nome, telefone);
+        this.salario = salario;
+    }
+
+    public String getDados() {
+        return super.getDados() + " - " + salario;
+    }
     
 }

9bc9747b0ae166f3e431639c2e0e78e8f0e38441
Joao <engjobe@gmail.com>
2021-12-13 22:54:14 -0300

    Bee1037

--- bee/Bee1037.java
+++ bee/Bee1037.java
@@ -3,10 +3,29 @@ import java.util.Scanner;
 public class Bee1037 { // Main
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        double valor;
+        double intervalo;
 
+        intervalo = entrada.nextDouble();
+
+        if (intervalo >= 0 && intervalo <= 25) {
+            System.out.println("Intervalo [0,25]");
+        } else {
+            if (intervalo > 25 && intervalo <= 50) {
+                System.out.println("Intervalo (25,50]");
+            } else {
+                if (intervalo > 50 && intervalo <= 75) {
+                    System.out.println("Intervalo (50,75]");
+                } else {
+                    if (intervalo > 75 && intervalo <= 100) {
+                        System.out.println("Intervalo (75,100]");
+                    } else {
+                        System.out.println("Fora de intervalo");
+
+                    }
+                }
+            }
+        }
 
         entrada.close();
     }
-    
 }

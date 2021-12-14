196224022c46d33349b35f4daab6c6a244dfd5d5
Joao <engjobe@gmail.com>
2021-12-13 21:55:01 -0300

    aula 10

---
+++ bee/Bee1037.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Bee1037 { // Main
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double valor;
+
+
+        entrada.close();
+    }
+    
+}
